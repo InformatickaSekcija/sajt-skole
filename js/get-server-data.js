5fa335661f3414f0eb3451559325e88695566928
liondj0 <liondj007@gmail.com>
2018-10-01 16:33:06 +0200

    Dodato kupljenje notifikacija, probni front end i search funkcija

--- js/get-server-data.js
+++ js/get-server-data.js
@@ -1,8 +1,11 @@
 var notifications = [];
 
 $(document).ready(function () {
+    //Fetching notifications from database
     $.get('/beckend-ljev/obavestenja.json', function (data) {
+        //adding a global containing notifications
         notifications = data;
+        //Appending a notification template for each notification
         notifications.forEach(function (data) {
             $('.notification-wrapper').append(returnNotificationsTemplate(data))
         })
